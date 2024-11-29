c4b18afe49d077df7293f2c93f3d59f6d7077f95
Yoonhwi <ush0105@naver.com>
2024-11-29 14:24:18 +0900

    added dark-mode

--- scripts/initialize.js
+++ scripts/initialize.js
@@ -9,6 +9,19 @@ export const domController = new DomController(userController);
   domController.init();
   const searchBtn = document.querySelector(".search-button");
   const searchInput = document.getElementById("search-input");
+  const darkModeBtn = document.querySelector(".darkmode-btn");
+
+  const theme = localStorage.getItem("theme");
+  theme === "dark" ? document.documentElement.classList.add("dark") : null;
+  darkModeBtn.addEventListener("click", () => {
+    if (document.documentElement.classList.contains("dark")) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    }
+  });
 
   searchBtn.addEventListener("click", async (e) => {
     e.preventDefault();
